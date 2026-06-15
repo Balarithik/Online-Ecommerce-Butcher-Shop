b9a8964eb169c3c60e1a2b0b396410dc30e0e388
balarithik <balarithik15@gmail.com>
2026-06-15 10:42:21 +0530

    updated admin_dashboard error

--- main/admin_dashboard/views.py
+++ main/admin_dashboard/views.py
@@ -8,21 +8,6 @@ from orders.models import Order
 
 
 
-products = Products.objects.all()
-orders = Order.objects.all()
-
-
-
-total_products = products.count()
-total_orders = orders.count()
-try :
-    orders_delivered = orders.filter(status='Delivered').count()
-except:
-    orders_delivered = None 
-try:
-    orders_pending = orders.filter(status='Pending').count()
-except:
-    orders_pending = None
 
 
 
@@ -41,11 +26,29 @@ def admin_login(request):
 @login_required
 @user_passes_test(lambda u: u.is_superuser, login_url='/admin_login/')
 def admin_dashboard(request):
+    products = Products.objects.all()
+    orders = Order.objects.all()
+
+
+
+    total_products = products.count()
+    total_orders = orders.count()
+    try :
+        orders_delivered = orders.filter(status='Delivered').count()
+    except:
+        orders_delivered = None 
+    try:
+        orders_pending = orders.filter(status='Pending').count()
+    except:
+        orders_pending = None
+
     return render(request, "admin/admin_dashboard.html", {'products': products, 'orders': orders, 'total_products': total_products, 'total_orders': total_orders, 'orders_delivered': orders_delivered, 'orders_pending': orders_pending})
 
 
 def admin_product(request):
+    products = Products.objects.all()
     return render(request, "admin/products.html",{'products':products})
 
 def admin_orders(request):
+    orders = Order.objects.all()
     return render(request, "admin/orders.html",{'orders':orders})
--- main/main/settings.py
+++ main/main/settings.py
@@ -87,8 +87,16 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
-DATABASES = {
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
