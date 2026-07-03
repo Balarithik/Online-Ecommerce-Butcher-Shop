8776af605e274754a609cc97177bd1ab97ceb517
balarithik <balarithik15@gmail.com>
2026-07-03 18:32:29 +0530

    updated build.sh to add superuser creation

--- main/build.sh
+++ main/build.sh
@@ -10,4 +10,4 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-python manage.py auto_createsuperuser --username admin --password admin123
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'balarithik15@gmal.com', 'admin123')" | python manage.py shell
